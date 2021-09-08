df3fd0eade19f1f59e2ce451f0390dfad5366c9f
Ilgsson Braga <ilgssonbraga@gmail.com>
2021-09-07 22:12:53 -0300

    feat: initial commit 🚀

---
+++ todo-server/src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn pong() -> impl Responder{
+    HttpResponse::Ok().body("pong")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/")
+                        .route("ping", web::get().to(pong))
+                        .route("", web::get().to(|| HttpResponse::NotFound())),   
+        )
+    })
+    .workers(6)
+    .bind("127.0.0.1:4000")
+    .unwrap()
+    .run()
+    .await
+}
